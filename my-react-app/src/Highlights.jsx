f1f4533672a1b167d4f93afbd232d05f0fccd645
diytramboss <diytramboss@outlook.com>
2025-11-12 22:02:03 +0700

    Mostly install and configure Tailwind to Storybook

--- my-react-app/src/Highlights.jsx
+++ my-react-app/src/Highlights.jsx
@@ -4,9 +4,10 @@ import './Highlights.css';
 
 export default function Highlights() {
     return (
-        <div className="highlights">
-            <h2>Highlights Component</h2>
-            <p>This is a placeholder for the Highlights component.</p>
+        <div className="highlights flex">
+            <div className="higlight-item"><iframe src="https://youtube.com/embed/2yJgwwDcgV8?muted=0&controls=0&autoplay=1" frameborder="0"></iframe></div>
+            <div className="higlight-item"><iframe src="https://youtube.com/embed/2yJgwwDcgV8?muted=0&controls=0&autoplay=1" frameborder="0"></iframe></div>
+            <div className="higlight-item"><iframe src="https://youtube.com/embed/2yJgwwDcgV8?muted=0&controls=0&autoplay=1" frameborder="0"></iframe></div>
         </div>
     );
 }

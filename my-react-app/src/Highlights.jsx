498b552d64be5532e766be6080acea6550cf28b9
diytramboss <diytramboss@outlook.com>
2025-11-12 22:06:59 +0700

    Bit More

--- my-react-app/src/Highlights.jsx
+++ my-react-app/src/Highlights.jsx
@@ -4,10 +4,10 @@ import './Highlights.css';
 
 export default function Highlights() {
     return (
-        <div className="highlights flex">
-            <div className="higlight-item"><iframe src="https://youtube.com/embed/2yJgwwDcgV8?muted=0&controls=0&autoplay=1" frameborder="0"></iframe></div>
-            <div className="higlight-item"><iframe src="https://youtube.com/embed/2yJgwwDcgV8?muted=0&controls=0&autoplay=1" frameborder="0"></iframe></div>
-            <div className="higlight-item"><iframe src="https://youtube.com/embed/2yJgwwDcgV8?muted=0&controls=0&autoplay=1" frameborder="0"></iframe></div>
+        <div className="highlights flex space-x-4 overflow-x-hidden p-4">
+            <div className="higlight-item"><iframe src="https://youtube.com/embed/2yJgwwDcgV8?playlist=2yJgwwDcgV8&loop=1muted=0&controls=0&autoplay=1" frameborder="0"></iframe></div>
+            <div className="higlight-item"><iframe src="https://youtube.com/embed/2yJgwwDcgV8?playlist=tgbNymZ7vqY&loop=1muted=0&controls=0&autoplay=1" frameborder="0"></iframe></div>
+            <div className="higlight-item"><iframe src="https://youtube.com/embed/2yJgwwDcgV8?playlist=2yJgwwDcgV8&loop=1muted=0&controls=0&autoplay=1" frameborder="0"></iframe></div>
         </div>
     );
 }
